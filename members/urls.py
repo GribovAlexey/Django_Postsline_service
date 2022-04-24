00bdb4cf8c544e909075f2fd38e7da23e2a49868
GribovAlexey <gribov.alexey.m@yandex.ru>
2022-04-24 18:16:31 +0300

    little edition

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,7 @@
 from .views import UserRegisterView
-from django.urls import path
-
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('register/', UserRegisterView.as_view(), name='register'),
 ]
--- src/urls.py
+++ src/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include("blog.urls")),
-    path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 ]
